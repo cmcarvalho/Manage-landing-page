487148fb17fc137929cb195c6073b2ad41e1864c
Cristiano Carvalho Machado <69367177+cmcarvalho@users.noreply.github.com>
2021-09-06 23:48:47 -0300

    Add files via upload

--- 015 - Manage landing page - FrontEnd Mentor/js/script.js
+++ 015 - Manage landing page - FrontEnd Mentor/js/script.js
@@ -1,6 +1,15 @@
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
 
-header.addEventListener("click", function () {
+btnHamburger.addEventListener("click", function () {
   header.classList.toggle("open");
+
+  if (header.classList.contains("open")) {
+    overlay.classList.add("fade-in");
+    overlay.classList.remove("fade-out");
+  } else {
+    overlay.classList.add("fade-out");
+    overlay.classList.remove("fade-in");
+  }
 });
